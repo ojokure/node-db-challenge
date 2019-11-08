e132e13141cd82b19a5f61dbe5c2b6938d498c2e
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-08 08:34:36 +0000

    created a Tables for all the resources

---
+++ Data/migrations/20191108082747_createTable.js
@@ -0,0 +1,71 @@
+
+exports.up = function(knex) {
+    return knex.schema
+      .createTable("projects", table => {
+        table.increments();
+        table
+          .string("project_name", 100)
+          .unique()
+          .notNullable();
+        table.string("project_desc", 360);
+        table
+          .boolean('completed')
+          .defaultTo(false)
+          .notNullable();
+      })
+      .createTable("tasks", table => {
+        table.increments();
+        table
+          .string("task_desc", 360)
+          .unique()
+          .notNullable();
+        table.string("notes", 100);
+        table
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("projects");
+        table
+          .boolean('completed')
+          .defaultTo(false)
+          .notNullable();
+      })
+      .createTable("resources", table => {
+        table.increments();
+        table
+          .string("resource_name", 100)
+          .notNullable()
+          .unique();
+        table.string("resource_desc");
+        table
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("projects");
+      })
+      .createTable("projectDetails", table => {
+        table.increments();
+        table
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("projects");
+        table
+          .integer("resource_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("resources");
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists("projects")
+      .dropTableIfExists("tasks")
+      .dropTableIfExists("resources")
+      .dropTableIfExists("projectDetails");
+  };
