dd59bc6e1a9f41038b91adcbbf1b1f2329b514b5
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-08 09:44:23 +0000

    created a findById helper function for resources endpoint

--- Projects/resources-model.js
+++ Projects/resources-model.js
@@ -2,10 +2,24 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   find,
+  findById,
+  add
 };
 
 function find() {
-  return db.select("id","resource_name","resource_desc").from('resources');
+  return db.select("id", "resource_name", "resource_desc").from("resources");
 }
 
-// select id, resource_name, resource_desc from resources
+function findById(id) {
+  return db("resources")
+    .where({ id })
+    .first();
+}
+
+function add(resource) {
+  return db("resources")
+    .insert(resource, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
+}
